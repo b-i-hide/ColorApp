5e7bb63aa15cb52c489d7704e4f1f1cd622b5598
b-i-hide <b.i1993patriots@gmail.com>
2017-03-05 23:47:23 +0900

    set tapBtn action

--- ColorApp/ViewController.swift
+++ ColorApp/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var colorView = CustomView()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.addSubview(colorView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapButton(_ sender: UIButton) {
+        let backgroundView = colorView.makeBackgroundView(viewController: self)
+        self.view.addSubview(backgroundView)
+        let colorSelectView = colorView.makeColorSelectView(bgView: backgroundView)
+        backgroundView.addSubview(colorSelectView)
+        for i in 1...9 {
+            var colors = ["red", "blue", "black", "green", "cyan", "yellow", "magenta","purple", "orange"]
+            let colorCell = colorView.makeColorCell(colorSelectView: colorSelectView, color: colors[i], x: 5, y: 5)
+            colorSelectView.addSubview(colorCell)
+        }
+    }
 
 }
 
